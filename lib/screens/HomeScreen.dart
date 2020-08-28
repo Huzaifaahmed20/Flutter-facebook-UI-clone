c3dd025ad3c774d0745c501e1940b836958f65ca
Huzaifa Ahmed <ahmedhuzaifa13@gmail.com>
2020-08-28 23:28:44 +0500

    make sliver app bar on home screen

--- lib/screens/HomeScreen.dart
+++ lib/screens/HomeScreen.dart
@@ -1,10 +1,44 @@
+import 'package:facebook_ui_clone/config/pallete.dart';
+import 'package:facebook_ui_clone/widgets/widgets.dart';
 import 'package:flutter/material.dart';
+import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({Key key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: CustomScrollView(
+        slivers: [
+          SliverAppBar(
+            brightness: Brightness.light,
+            backgroundColor: Colors.white,
+            title: Text(
+              'facebook',
+              style: const TextStyle(
+                color: Palette.facebookBlue,
+                fontSize: 28.0,
+                fontWeight: FontWeight.bold,
+                letterSpacing: -1.2,
+              ),
+            ),
+            floating: true,
+            actions: [
+              CircleButton(
+                icon: Icons.search,
+                iconSize: 30.0,
+                onPressed: () {},
+              ),
+              CircleButton(
+                icon: MdiIcons.facebookMessenger,
+                iconSize: 30.0,
+                onPressed: () {},
+              ),
+            ],
+          )
+        ],
+      ),
+    );
   }
 }
